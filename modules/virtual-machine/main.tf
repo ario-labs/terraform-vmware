6e8f392eb1c59280bee8a3fa1ed74fbb5d9b77dd
Nick Arellano <nick.a@hey.com>
2024-06-26 11:01:31 -0500

    feat: add Terraform configuration for vSphere virtual machines
    - Create .gitignore to exclude sensitive files and directories
    - Add main Terraform configuration for vSphere provider and virtual machine module
    - Implement virtual machine module with resources and data sources for vSphere
    - Define input variables for virtual machine module and main configuration
    - Specify required provider versions for vSphere in versions.tf files

---
+++ modules/virtual-machine/main.tf
@@ -0,0 +1,73 @@
+data "vsphere_datacenter" "datacenter" {
+  name = var.datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+  name          = var.datastore
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+  name          = var.cluster
+}
+
+data "vsphere_network" "network" {
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+  name          = var.network
+}
+
+data "vsphere_virtual_machine" "template" {
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+  name          = var.template
+}
+
+locals {
+  is_windows_template = regex("(?i)windows", data.vsphere_virtual_machine.template.guest_id) != null
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.name
+  datastore_id     = data.vsphere_datastore.datastore.id
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  num_cpus         = var.num_cpus
+  memory           = var.memory
+  folder           = var.folder
+  firmware         = "efi"
+  lifecycle {
+    ignore_changes = [hv_mode, ept_rvi_mode]
+  }
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    thin_provisioned = true
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    linked_clone  = var.linked_clone
+    customize {
+      dynamic "windows_options" {
+        for_each = local.is_windows_template ? [1] : []
+        content {
+          computer_name = var.name
+        }
+      }
+      dynamic "linux_options" {
+        for_each = local.is_windows_template ? [] : [1]
+        content {
+          host_name = var.name
+          domain    = ""
+        }
+      }
+      network_interface {
+        ipv4_address = split("/", var.ipv4_with_cidr)[0]
+        ipv4_netmask = split("/", var.ipv4_with_cidr)[1]
+      }
+      ipv4_gateway = cidrhost(var.ipv4_with_cidr, 1)
+    }
+  }
+}
