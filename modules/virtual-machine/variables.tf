bbf3940df8c54b7d5dbbb938818b05f2435602ed
Nick Arellano <nick.a@hey.com>
2024-06-26 11:02:17 -0500

    feat: add Terraform configuration for vSphere virtual machines (#1)
    - Create .gitignore to exclude sensitive files and directories
    - Add main Terraform configuration for vSphere provider and virtual machine module
    - Implement virtual machine module with resources and data sources for vSphere
    - Define input variables for virtual machine module and main configuration
    - Specify required provider versions for vSphere in versions.tf files

---
+++ modules/virtual-machine/variables.tf
@@ -0,0 +1,56 @@
+variable "name" {
+  description = "The name of the virtual machine"
+  type        = string
+}
+
+variable "datacenter" {
+  description = "The datacenter to use"
+  type        = string
+}
+
+variable "datastore" {
+  description = "The datastore to use"
+  type        = string
+}
+
+variable "cluster" {
+  description = "The cluster to use"
+  type        = string
+}
+
+variable "network" {
+  description = "The network to use"
+  type        = string
+}
+
+variable "num_cpus" {
+  description = "The number of CPUs to allocate"
+  type        = number
+}
+
+variable "memory" {
+  description = "The amount of memory to allocate"
+  type        = number
+}
+
+variable "folder" {
+  description = "The folder to place the virtual machine in"
+  type        = string
+}
+
+variable "template" {
+  description = "The template to clone"
+  type        = string
+}
+
+variable "linked_clone" {
+  description = "Whether to create a linked clone"
+  type        = bool
+  default     = true
+}
+
+variable "ipv4_with_cidr" {
+  description = "The IP address of the virtual machine with the cidr"
+  type        = string
+  default     = null
+}
